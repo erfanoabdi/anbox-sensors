631a1b41dc01f15e7dab4b55c28250a1e945828b
Erfan Abdi <erfangplus@gmail.com>
2021-05-15 19:22:53 +0430

    all others

--- Sensors.cpp
+++ Sensors.cpp
@@ -99,8 +99,10 @@ static int sensor_device_poll_event_locked(SensorDevice* dev)
 
     int64_t event_time = -1;
     int ret = 0;
-    quint64 ts;
-    int x, y, z;
+    uint64_t ts;
+    int x, y, z, rx, ry, rz, tmp;
+    unsigned value;
+    bool isNear;
 
     /* Release the lock since we're going to block on recv() */
     pthread_mutex_unlock(&dev->lock);
@@ -112,14 +114,103 @@ static int sensor_device_poll_event_locked(SensorDevice* dev)
     // do not overwrite it. We can resume saving sensor
     // values after that meta data has been received.
 
-    /* "acceleration:<x>:<y>:<z>" corresponds to an acceleration event */
     if (dev->mSensorFWDevice->GetAccelerometerEvent(&ts, &x, &y, &z) == 0) {
-        new_sensors |= SENSORS_ACCELEROMETER;
-        events[ID_ACCELEROMETER].u.vec3.x = x / 100.00f;
-        events[ID_ACCELEROMETER].u.vec3.y = y / 100.00f;
-        events[ID_ACCELEROMETER].u.vec3.z = z / 100.00f;
-        events[ID_ACCELEROMETER].u.vec3.status = SensorStatus::ACCURACY_MEDIUM;
-        events[ID_ACCELEROMETER].sensorType = SensorType::ACCELEROMETER;
+        if (ts != last_TimeStamp[ID_ACCELEROMETER]) {
+            new_sensors |= SENSORS_ACCELEROMETER;
+            events[ID_ACCELEROMETER].u.vec3.x = x / 100.00f;
+            events[ID_ACCELEROMETER].u.vec3.y = y / 100.00f;
+            events[ID_ACCELEROMETER].u.vec3.z = z / 100.00f;
+            events[ID_ACCELEROMETER].u.vec3.status = SensorStatus::ACCURACY_MEDIUM;
+            events[ID_ACCELEROMETER].sensorType = SensorType::ACCELEROMETER;
+            last_TimeStamp[ID_ACCELEROMETER] = ts;
+        }
+    }
+    if (dev->mSensorFWDevice->GetGyroscopeEvent(&ts, &x, &y, &z) == 0) {
+        if (ts != last_TimeStamp[ID_GYROSCOPE]) {
+            new_sensors |= SENSORS_GYROSCOPE;
+            events[ID_GYROSCOPE].u.vec3.x = x / 100.00f;
+            events[ID_GYROSCOPE].u.vec3.y = y / 100.00f;
+            events[ID_GYROSCOPE].u.vec3.z = z / 100.00f;
+            events[ID_GYROSCOPE].u.vec3.status = SensorStatus::ACCURACY_MEDIUM;
+            events[ID_GYROSCOPE].sensorType = SensorType::GYROSCOPE;
+            last_TimeStamp[ID_GYROSCOPE] = ts;
+        }
+    }
+    if (dev->mSensorFWDevice->GetHumidityEvent(&ts, &value) == 0) {
+        if (ts != last_TimeStamp[ID_HUMIDITY]) {
+            new_sensors |= SENSORS_HUMIDITY;
+            events[ID_HUMIDITY].u.scalar = value;
+            events[ID_HUMIDITY].sensorType = SensorType::RELATIVE_HUMIDITY;
+            last_TimeStamp[ID_HUMIDITY] = ts;
+        }
+    }
+    if (dev->mSensorFWDevice->GetLightEvent(&ts, &value) == 0) {
+        if (ts != last_TimeStamp[ID_LIGHT]) {
+            new_sensors |= SENSORS_LIGHT;
+            events[ID_LIGHT].u.scalar = value;
+            events[ID_LIGHT].sensorType = SensorType::LIGHT;
+            last_TimeStamp[ID_LIGHT] = ts;
+        }
+    }
+    if (dev->mSensorFWDevice->GetMagnetometerEvent(&ts, &x, &y, &z, &rx, &ry, &rz, &tmp) == 0) {
+        if (ts != last_TimeStamp[ID_MAGNETIC_FIELD]) {
+            new_sensors |= SENSORS_MAGNETIC_FIELD;
+            events[ID_MAGNETIC_FIELD].u.vec3.x = x;
+            events[ID_MAGNETIC_FIELD].u.vec3.y = y;
+            events[ID_MAGNETIC_FIELD].u.vec3.z = z;
+            events[ID_MAGNETIC_FIELD].u.vec3.status = SensorStatus::ACCURACY_HIGH;
+            events[ID_MAGNETIC_FIELD].sensorType = SensorType::MAGNETIC_FIELD;
+            last_TimeStamp[ID_MAGNETIC_FIELD] = ts;
+        }
+        if (ts != last_TimeStamp[ID_MAGNETIC_FIELD_UNCALIBRATED]) {
+            new_sensors |= SENSORS_MAGNETIC_FIELD_UNCALIBRATED;
+            events[ID_MAGNETIC_FIELD_UNCALIBRATED].u.vec3.x = rx;
+            events[ID_MAGNETIC_FIELD_UNCALIBRATED].u.vec3.y = ry;
+            events[ID_MAGNETIC_FIELD_UNCALIBRATED].u.vec3.z = rz;
+            events[ID_MAGNETIC_FIELD_UNCALIBRATED].u.vec3.status = SensorStatus::ACCURACY_HIGH;
+            events[ID_MAGNETIC_FIELD_UNCALIBRATED].sensorType = SensorType::MAGNETIC_FIELD;
+            last_TimeStamp[ID_MAGNETIC_FIELD_UNCALIBRATED] = ts;
+        }
+    }
+    if (dev->mSensorFWDevice->GetOrientationEvent(&ts, &tmp) == 0) {
+        if (ts != last_TimeStamp[ID_DEVICE_ORIENTATION]) {
+            new_sensors |= SENSORS_DEVICE_ORIENTATION;
+            events[ID_DEVICE_ORIENTATION].u.scalar = tmp;
+            events[ID_DEVICE_ORIENTATION].sensorType = SensorType::DEVICE_ORIENTATION;
+            last_TimeStamp[ID_DEVICE_ORIENTATION] = ts;
+        }
+    }
+    if (dev->mSensorFWDevice->GetPressureEvent(&ts, &value) == 0) {
+        if (ts != last_TimeStamp[ID_PRESSURE]) {
+            new_sensors |= SENSORS_PRESSURE;
+            events[ID_PRESSURE].u.scalar = value;
+            events[ID_PRESSURE].sensorType = SensorType::PRESSURE;
+            last_TimeStamp[ID_PRESSURE] = ts;
+        }
+    }
+    if (dev->mSensorFWDevice->GetProximityEvent(&ts, &value, &isNear) == 0) {
+        if (ts != last_TimeStamp[ID_PROXIMITY]) {
+            new_sensors |= SENSORS_PROXIMITY;
+            events[ID_PROXIMITY].u.scalar = value;
+            events[ID_PROXIMITY].sensorType = SensorType::PROXIMITY;
+            last_TimeStamp[ID_PROXIMITY] = ts;
+        }
+    }
+    if (dev->mSensorFWDevice->GetStepcounterEvent(&ts, &value) == 0) {
+        if (ts != last_TimeStamp[ID_STEPCOUNTER]) {
+            new_sensors |= SENSORS_STEPCOUNTER;
+            events[ID_STEPCOUNTER].u.stepCount = value;
+            events[ID_STEPCOUNTER].sensorType = SensorType::STEP_COUNTER;
+            last_TimeStamp[ID_STEPCOUNTER] = ts;
+        }
+    }
+    if (dev->mSensorFWDevice->GetTemperatureEvent(&ts, &value) == 0) {
+        if (ts != last_TimeStamp[ID_TEMPERATURE]) {
+            new_sensors |= SENSORS_TEMPERATURE;
+            events[ID_TEMPERATURE].u.scalar = value;
+            events[ID_TEMPERATURE].sensorType = SensorType::AMBIENT_TEMPERATURE;
+            last_TimeStamp[ID_TEMPERATURE] = ts;
+        }
     }
 
     if (new_sensors) {
